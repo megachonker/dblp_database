1bc22967809b3f57dff529150de3e7d47272a2c3
Konqie <Konqie@chonky.fr>
2022-01-11 19:50:45 +0100

    ajout de fonction macro  utilisable partout

---
+++ main/macro.h
@@ -0,0 +1,22 @@
+
+macro variadique
+#define my_printf(...) printf(__VA_ARGS__)
+
+
+couleur
+
+BLEUX()
+Printf(message)
+CLR()
+
+BLEUX(STRING)
+
+
+DEBUG variable                                               GREEN
+INFO   etape clef programe                                             BLUE
+WARNING INFO DEBUG                                 YELLO
+ERROR => redefinition de exitifnull et de fprint errd RED
+
+
+
+exitiffnull si 0 ou NULL un vrais exit un print des message avec error
