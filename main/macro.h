bc568aefd37bac00b598167bf92c503c5a286d75
Konqie <Konqie@chonky.fr>
2022-01-12 15:41:53 +0100

    ajout exitnull et fprint

--- main/macro.h
+++ main/macro.h
@@ -1,4 +1,4 @@
-
+#include <stdlib.h>
 
 #define DEBUG_ON
 #define INFO_ON
@@ -15,38 +15,33 @@
 #define customxml    "DATA/custom.xml"
 
 //macro pour les couleur
-#define GREEN() printf("\033[1;32m");
-#define BLUE()  printf("\033[1;34m");
-#define RED()   printf("\033[1;31m"); //faire un fprint
-#define CLR()   printf("\033[0m");
+#define GREEN() fprintf(stderr,"\033[1;32m");
+#define BLUE()  fprintf(stderr,"\033[1;34m");
+#define RED()   fprintf(stderr,"\033[1;31m"); //faire un fprint
+#define CLR()   fprintf(stderr,"\033[0m");
 
 //definition des marcro variadique
 
 #ifdef DEBUG_ON
-#define DEBUG(...) GREEN() printf (__VA_ARGS__); CLR() printf("\n");
+#define DEBUG(...) GREEN() fprintf(stderr, __VA_ARGS__); CLR() fprintf(stderr,"\n");
 #else
 #define DEBUG(TEST) ;
 #endif
 
 #ifdef INFO_ON
-#define INFO(...) BLUE() printf (__VA_ARGS__); CLR() printf("\n");
+#define INFO(...) BLUE() fprintf(stderr, __VA_ARGS__); CLR() fprintf(stderr,"\n");
 #else
 #define INFO(TEST) ;
 #endif
 // #ifdef WARN_ON
-// WARNING() INFO DEBUG                                 YELLO
+// WARNING() INFO DEBUG       PROGRESS BAR?                          YELLO
 // #endif
 
-#define ERROR(...) RED() printf (__VA_ARGS__); CLR() printf("\n");//faire un fprint
+#define ERROR(...) RED() fprintf(stderr, __VA_ARGS__); CLR() fprintf(stderr,"\n");//faire un fprint
 
-//a Pimper la fonction
-
-#define exitIfNull(p,msg)\
+#define exitIfNull(p,...)\
 if (!p)\
 {\
-    fprintf(stderr,msg);\
-}\
-
-
-
-// exitiffnull si 0 ou NULL un vrais exit un print des message avec error
+    ERROR(__VA_ARGS__)\
+    exit(1);\
+}
