eb7f59e38f2a93f6ddb657dfd7111d1a60d94edb
Katie <Katie@live.fr>
2022-01-16 17:30:07 +0100

    mise a jour Dijkstra, j'ai fait une règle dans le Makefile aussi

--- main/Dijkstra.h
+++ main/Dijkstra.h
@@ -23,13 +23,14 @@ typedef enum comparaison_auteur_2
 typedef struct plus_court_chemin_struct
 {
     auteur_struct** pcc_tab_ptr_auteur; //tableau de pointeur vers les auteurs ai, du plus court chemin pour arriver à l'auteur ai
+    int size_pcc_auteur;
     Article_struct** pcc_tab_ptr_Article; //tableau de pointeur vers les Articles Ai, du plus court chemin pour arriver à l'auteur ai (l'auteur d'indice i dans pcc_tab_ptr_auteur)
+    int size_pcc_Article;
 }plus_court_chemin_struct;
 
 
+plus_court_chemin_struct* Dijkstra(graphe_struct graphe_struct, char* nom_auteur_1, char* nom_auteur_2);
 
-plus_court_chemin_struct* Dijkstra(auteur_struct **graphe, int* size_graphe_ptr, char* nom_auteur_1, char* nom_auteur_2);
-
-void free_Dijkstra(auteur_struct** graphe, int size_graphe, plus_court_chemin_struct *pcc_ptr, int size_pcc_auteur, int size_pcc_Article);
+void free_Dijkstra(graphe_struct* graphe_struct, plus_court_chemin_struct *pcc_ptr);
 
 #endif
